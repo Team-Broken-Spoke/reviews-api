7296ef7db76c47fcf75f36b48c9dc2ac9855f714
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-10-16 17:08:24 -0500

    Create convertFullStream to convert json data to formatted json data to prepare for mongodb import

---
+++ convertFullStream.js
@@ -0,0 +1,63 @@
+const csv = require('csvtojson')
+const fs = require('fs');
+const readline = require('readline');
+
+
+// to convert csv to JSON:
+// let writeStream = fs.createWriteStream('../data-csvs/fullData.json');
+// let readStream = fs.createReadStream('../data-csvs/reviews.csv')
+// readStream.pipe(csv({downstreamFormat: 'array'})).pipe(writeStream);
+
+
+let writeStream = fs.createWriteStream('../data-csvs/fullConvertedData.json');
+let readStream = fs.createReadStream('../data-csvs/fullData.json');
+
+
+const reviewDetails = (review) => {
+  return {
+    "review_id": review.id,
+    "rating": review.rating,
+    "summary": review.summary,
+    "recommend": review.recommend,
+    "response": review.response,
+    "body": review.body,
+    "date": review.date,
+    "reviewer_name": review.reviewer_name,
+    "helpfulness": review.helpfulness
+  }
+}
+
+let currentId = "1";
+let obj = {
+  "product_id" : "1",
+  "results": []
+}
+
+const processRecord = (data) => {
+  // writeStream.write(data.summary)
+  if(data.product_id !== currentId) {
+    obj["results"].push(reviewDetails(data));
+    writeStream.write(JSON.stringify(obj))
+    obj = { "product_id": data.product_id, "results": [] }
+    currentId = data.product_id;
+  } else {
+    obj["results"].push(reviewDetails(data));
+  }
+}
+// read line-by-line
+const readInterface = readline.createInterface({
+  input: readStream
+});
+readInterface.on('line', (line) => {
+  line = line.trim();
+
+  if (line.charAt(line.length-1) === ',') {
+      line = line.substr(0, line.length-1);
+  }
+
+  if (line.charAt(0) === '{') {
+      processRecord(JSON.parse(line));
+  }
+});
+
+
